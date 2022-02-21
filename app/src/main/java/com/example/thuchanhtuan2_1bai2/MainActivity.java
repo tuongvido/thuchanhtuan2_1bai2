cdd6b2ed7907fbc012f7d20a0de506a8a358fb7f
dothituongvi1812 <dothituongviplic@gmail.com>
2022-02-21 15:58:52 +0700

    convert to C&F

--- app/src/main/java/com/example/thuchanhtuan2_1bai2/MainActivity.java
+++ app/src/main/java/com/example/thuchanhtuan2_1bai2/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.thuchanhtuan2_1bai2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.fonts.Font;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
         EditText edtdoF=findViewById(R.id.edtdoF);
         EditText edtdoC=findViewById(R.id.edtdoC);
 
+        TextView tvThongBaoF=findViewById(R.id.tvThongBaoF);
+        TextView tvThongBaoC=findViewById(R.id.tvThongBaoC);
         Button btnConvToF=findViewById(R.id.buttonConvToF);
         Button btnConvToC=findViewById(R.id.buttonConvToC);
         Button btnClear=findViewById(R.id.buttonClear);
@@ -23,19 +29,34 @@ public class MainActivity extends AppCompatActivity {
         btnConvToF.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String doC=edtdoC.getText().toString();
+                if(!doC.matches("^[0-9]+$") ||doC.equals("")){
+                    tvThongBaoC.setText("Vui lòng nhập số");
+                    edtdoC.selectAll();
+                    edtdoC.setFocusable(true);
 
-                double doC=Double.valueOf(edtdoC.getText().toString());
-
-                double doF=((float)9/5)*doC+32;
+                    return;
+                }
+                tvThongBaoC.setText("");
+                double c=Double.parseDouble(edtdoC.getText().toString());
+                double doF=((float)9/5)*c+32;
                 edtdoF.setText(String.valueOf(doF));
             }
         });
         btnConvToC.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String doF=edtdoF.getText().toString();
+                if(!doF.matches("^[0-9]+$") ||doF.equals("")) {
+                    tvThongBaoF.setText("Vui lòng nhập số");
+                    edtdoF.selectAll();
+                    edtdoF.setFocusable(true);
 
-                double doF=Double.valueOf(edtdoF.getText().toString());
-                double doC=((float)5/9)*(doF-32);
+                    return;
+                }
+                tvThongBaoF.setText("");
+                double f=Double.parseDouble(edtdoF.getText().toString());
+                double doC=((float)5/9)*(f-32);
                 edtdoC.setText(String.valueOf(doC));
             }
         });
@@ -44,6 +65,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 edtdoC.setText("");
                 edtdoF.setText("");
+                tvThongBaoC.setText("");
+                tvThongBaoF.setText("");
+
             }
         });
     }
